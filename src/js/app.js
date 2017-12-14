c561a7cddea4ab61f22588d5fe33cec367177ef6
ShikalovNikita <nshikalov@outlook.com>
2017-12-14 16:34:51 +0300

    cov5

--- src/js/app.js
+++ src/js/app.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
 	var itemBox = document.querySelectorAll('.catalog__item'),
 		cartContainer = document.getElementById('cart');
 
-	function addEvent(elem, type, handler) {
-		if (elem.addEventListener) {
-			elem.addEventListener(type, handler, false);
-		} else {
-			elem.attachEvent('on'+type, function() { handler.call(elem); });
-		}
+	function addEvent(elem, type, handler){
+	// if(elem.addEventListener){
+		elem.addEventListener(type, handler, false);
+		// } else {
+		// elem.attachEvent('on'+type, function(){ handler.call( elem ); });
+		// }
 		return false;
 	}
 
@@ -32,13 +32,13 @@ $(document).ready(function() {
 			totalPrice = 0,
 			totalCount = 0;
 		if(cartData.hasOwnProperty(itemId)) {
-			cartData[itemId][3] += 1;
+			cartData[itemId][2] += 1;
 		} else {
-			cartData[itemId] = [itemImg, itemTitle, itemPrice, 1];
+			cartData[itemId] = [itemImg, itemTitle, 1, itemPrice];
 		}
 		for(var items in cartData) {
-			totalPrice += Number(cartData[items][2]) * Number(cartData[items][3]);
-			totalCount += Number(cartData[items][3]);
+			totalPrice += Number(cartData[items][3]) * Number(cartData[items][2]);
+			totalCount += Number(cartData[items][2]);
 		}
 		$('.cartCounter').html(totalCount);
 		$('.cartSumma').html(totalPrice);
@@ -72,14 +72,17 @@ $(document).ready(function() {
 	                   $(this).remove();
 			});
 	});
+
 	function openCart(e) {
 		var cartData = getCartData(),
 			totalItems = '',
 			totalPrice = 0;
 		if(cartData !== null) {
-			totalItems = '<div class="popup__content__table">Товар</div><table>';
+			totalItems = '<div class="popup__content__list">'+
+							'<table class="popup__content__list__table">'+
+								'<tr class="popup__content__list__table-head"><td>Товар</td><td>Название</td><td>Кол-во</td><td>Цена</td></tr>';
 			for(var items in cartData) {
-				totalPrice += Number(cartData[items][1]) * Number(cartData[items][2]);
+				totalPrice += Number(cartData[items][3]) * Number(cartData[items][2]);
 				totalItems += '<tr>';
 				for(var i = 0; i < cartData[items].length; i++) {
 					totalItems += '<td>' + cartData[items][i] + '</td>';
@@ -87,26 +90,65 @@ $(document).ready(function() {
 				totalItems += '</tr>';
 			}
 			totalItems += '</table>'+
-										'<div class="zakaz_summa">'+
-											'<p><strong>Сумма заказа:</strong> '+totalPrice+'р.</p>'+
-											'<p><span>Доставка:</span> 200р.</p>'+
-											'<div class="itog_summa">'+
-												'<strong>Итого:</strong> '+(totalPrice+200)+
-											'</div>'+
-										'</div>';
+							'<div class="popup__content__list__description">Ваши товары хранятся в корзине более 30 дней. Вы можете продолжить покупки в любое удобное для вас время.</div>'+
+							'<button onclick="localStorage.removeItem(\'cart\'); location.reload();" class="popup__content__list__clearCart">Очистить корзину</button>'+
+						'</div>'+
+						'<div class="popup__content__price">'+
+							'<table>'+
+								'<tr><td>Товары</td><td>'+totalPrice+'р.</td></tr>'+
+								'<tr><td>Стоимость доставки</td><td>200р.</td></tr>'+
+								'<tr class="result_sum"><td>Итого</td><td>'+(totalPrice+200)+'р.</td></tr>'+
+							'</table>'+
+							'<div id="submitOrder" class="submit_order">Оформить заказ</div>'+
+						'</div>';
 			$('.popup__content').html(totalItems);
+
+			$('.submit_order').on('click', function() {
+				$('.popup__content').hide();
+				$('.popup__order').show();
+				$('.popup__order__summa').html('<div class="popup__content__price">'+
+							'<table>'+
+								'<tr><td>Товары</td><td>'+totalPrice+'р.</td></tr>'+
+								'<tr><td>Стоимость доставки</td><td>200р.</td></tr>'+
+								'<tr class="result_sum"><td>Итого</td><td>'+(totalPrice+200)+'р.</td></tr>'+
+							'</table>'+
+							'<button id="compliteOrder" class="submit_order">Оформить заказ</button>'+
+						'</div>');
+				$('#compliteOrder').on('click', function() {
+					var client = $("#clientInformation").serializeArray(), indexed_array = {};
+					$.map(client, function(n, i){
+						indexed_array[n['name']] = n['value'];
+					});
+					$.ajax({
+						url: '/sendOrder.php',
+						dataType: 'json',
+						type: 'post',
+						data: $.extend(indexed_array, cartData),
+						success: function(response) {
+							if(response.good) {
+								$('.popup__order').html('<div class="swal2-icon swal2-success swal2-animate-success-icon" style="display: block;"><div class="swal2-success-circular-line-left" style="background: rgb(255, 255, 255);"></div><span class="swal2-success-line-tip swal2-animate-success-line-tip"></span> <span class="swal2-success-line-long swal2-animate-success-line-long"></span><div class="swal2-success-ring"></div> <div class="swal2-success-fix" style="background: rgb(255, 255, 255);"></div><div class="swal2-success-circular-line-right" style="background: rgb(255, 255, 255);"></div></div><p>Ваш заказ отправлен, ожидайте скоро с Вами свяжется менеджер.</p>');
+								localStorage.removeItem('cart');
+							} else {
+								$('.popup__order').html('<div class="swal2-icon swal2-error"><span class="swal2-x-mark"><span class="swal2-x-mark-line-left"></span><span class="swal2-x-mark-line-right"></span></span></div><p>Ваш заказ не получилось отправить, позвоните нам 8 (909) 700-21-38 и мы решим это!</p>');
+							}
+						}
+					})
+				});
+			});
+			
 			// cartContainer.innerHTML = totalItems;
 		} else {
-			cartContainer.innerHTML = "В корзине пусто!";
+			$('.popup__content').html("В корзине пусто!");
 		}
 		return false;
 	}
 
 	addEvent(document.getElementById('checkout'), 'click', openCart);
+
 	var cartData = getCartData() || {}, totalCount = 0, totalPrice = 0;
 	for(var items in cartData) {
-		totalPrice += Number(cartData[items][2]) * Number(cartData[items][3]);
-		totalCount += Number(cartData[items][3]);
+		totalPrice += Number(cartData[items][3]) * Number(cartData[items][2]);
+		totalCount += Number(cartData[items][2]);
 	}
 	$('.cartCounter').html(totalCount);
 	$('.cartSumma').html(totalPrice);
@@ -117,6 +159,7 @@ $(document).ready(function() {
 		adaptiveHeight: true,
 		autoplay: true
 	});
+
 	$('#checkout').on('click', function(){
 		$('.popup').css({
 	'transform':'translate(0%, 0%)',
@@ -143,4 +186,20 @@ $(document).ready(function() {
 		});
 	});
 	});
+
+	$("#menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+
 });
